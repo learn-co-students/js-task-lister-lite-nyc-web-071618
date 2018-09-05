bd9a6e20df2f9157c89eb5ad1ffefe91a50a800c
github username <github email address>
2018-09-05 14:58:30 -0400

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,24 @@
-class Task {
-}
+document.addEventListener("DOMContentLoaded", function(event){
+  const searchBar = document.getElementById('new-task-description')
+
+  const createButton = document.getElementById('btn')
+  createButton.addEventListener('click', function(event){
+    event.preventDefault()
+    let taskContainer = document.getElementById('tasks')
+    let liItem = document.createElement('li')
+    liItem.innerText = searchBar.value
+    liItem.innerHTML += ` <button id='delete-btn' type="button">X</button>`
+    taskContainer.appendChild(liItem)
+
+    const deleteButton = document.getElementById('delete-btn')
+    liItem.addEventListener('click', function(event){
+      if(event.target.tagName === 'BUTTON') {
+        event.target.parentNode.remove()
+      }
+    })
+    //resets searchBar value each time
+    searchBar.value = ""
+
+  })
+
+})
